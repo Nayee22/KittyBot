8aeaa6b6feb8ba0572f7caa996277aa6ffcaf01a
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2023-11-17 21:25:21 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,13 +6,13 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*Mensaje:* ${pesan}`
-let teks = `*⛊──⛾「 Tag All 」⛾──⛊*\n*│*\n*│➭*  ${oi}\n*│*\n`
+let teks = `*⛊──⛾「 ⊹ ִֶָ 🩷 𝗞𝗶𝘁𝘁𝘆 𝗯𝗼𝘁 𝗹𝗼𝘀 𝗶𝗻𝘃𝗼𝗰𝗮   」⛾──⛊*\n*│*\n*│➭*  ${oi}\n*│*\n`
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
 for (let username of participants) {
-teks += `*│➭* @${username.id.split('@')[0]}\n`}
-teks += `*⛊──⛾「 Tag All 」⛾──⛊*`
+teks += `*⊹ ִֶָ 😼»* @${username.id.split('@')[0]}\n`}
+teks += `*⛊──⛾「 @kitty_botcita 」⛾──⛊*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 
 }
